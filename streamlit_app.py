6ae329b8549188e54a73ab846dc04f45d1532e07
jkrishha <jkrisha1473@gmail.com>
2024-08-11 00:27:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ st.write('this is a machine learning app')
 with st.expander('Data'):
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+st.write('X')
+X=df.drop('species',axis=1)
+X
+Y=df.species
+Y
